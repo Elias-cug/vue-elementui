31503bde48b40701631cdc7a08fe4ff322f61b1a
elias_lee <liyisheng0113@163.com>
2021-05-18 20:23:57 +0800

    增加iframe

---
+++ src/components/test/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="test-iframe">
+    <el-button @click="setCookie">设置cookie</el-button>
+    <el-button @click="getCookie">获取cookie</el-button>
+    <el-button @click="cleanCookie">清除cookie</el-button>
+    <span>{{ 'id' + id }}</span>
+    <span>{{ 'name' + name }}</span>
+  </div>
+</template>
+
+<script>
+import cookie from 'js-cookie'
+export default {
+  name: 'Test',
+  components: {},
+  data () {
+    return {
+      id: '',
+      name: ''
+    }
+  },
+  methods: {
+    setCookie () {
+      cookie.set('id', '0111111')
+      cookie.set('name', 'name')
+    },
+    getCookie () {
+      this.id = cookie.get('id')
+      this.name = cookie.get('name')
+    },
+    cleanCookie () {
+      this.id = ''
+      this.name = ''
+      cookie.remove(this.id)
+      cookie.remove(this.name)
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/router/moudle/index.js
+++ src/router/moudle/index.js
@@ -1,5 +1,8 @@
+import Layout from '@/layout'
 // 首页
 import Main from '@/page/Main'
+// 测试路由
+import Test from '@/components/test'
 // 可视化路由
 import visRouter from './visRouter'
 // 外部路由
@@ -8,6 +11,21 @@ const routes = [
   {
     path: '/',
     component: Main
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    redirect: '/test',
+    component: Layout,
+    meta: { title: '测试' },
+    children: [
+      {
+        path: '/test/iframe',
+        name: 'G6',
+        component: Test,
+        meta: { title: '测试cookie' }
+      }
+    ]
   },
   ...visRouter,
   ...outerRouter

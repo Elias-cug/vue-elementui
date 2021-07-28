2777454fbed234894739b8eb3dea3c6e1ebd2783
liyisheng@bonree.com <liyisheng@bonree.com>
2021-07-28 18:32:07 +0800

    删除无用注释

--- src/components/visualization/AntvG/AntvG/register-node.js
+++ src/components/visualization/AntvG/AntvG/register-node.js
@@ -5,10 +5,11 @@
 export function registerNode (G6) {
   registerRateNode(G6)
 }
-// path: `M ${x}, ${y}
-//   m -${r}, 0
-//   a ${r},${r} 0 1,0 ${r * 2},0
-//   a ${r},${r} 0 1,0 ${-r * 2},0`,
+
+/**
+ * 注册一个节点：实现按照比率展示圆环的颜色
+ * @param {*} G6
+ */
 function registerRateNode (G6) {
   G6.registerNode(
     'rate-node',
@@ -23,22 +24,15 @@ function registerRateNode (G6) {
 
         // 画大弧还是小弧 0：小 1：大
         const rightLargeArcFlag = rightRate >= 0.5 ? 1 : 0
-
         const falseLargeArcFlag = rightRate < 0.5 ? 1 : 0
-
-        // 顺时针还是逆时针 0：逆时针 1：顺时针
-        // const sweepFlag = ''
 
         //极坐标转换成直角坐标
         const xEnd = (r * Math.sin(rad)).toFixed(2)
         const yEnd = -(r * Math.cos(rad)).toFixed(2)
 
-        console.log('起点坐标是：')
-        console.log(0, -r)
-        console.log('终点坐标是：')
-        console.log(xEnd, yEnd)
-
+        // 正确的路径
         const rightPath = `M 0 ${-r} A ${r} ${r} 0 ${rightLargeArcFlag} 1 ${xEnd} ${yEnd}`
+        // 错误的路径
         const falsePath = `M 0 ${-r} A ${r} ${r} 0 ${falseLargeArcFlag} 0 ${xEnd} ${yEnd}`
 
         group.addShape('path', {
